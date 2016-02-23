b0ced54ff7108710f146b5592edfb0c5e0208805
Zac Hester <zac.hester@gmail.com>
2016-02-23 10:41:23 -0600

    Adding package setup script.

---
+++ setup.py
@@ -0,0 +1,44 @@
+#=============================================================================
+#
+# aptask Package Setup Script
+#
+#=============================================================================
+
+"""
+aptask Package Setup Script
+===========================
+
+Normal Installation
+-------------------
+
+    python setup.py install
+
+Development Installation
+------------------------
+
+    python setup.py develop
+
+Create Source Distribution
+--------------------------
+
+    python setup.py sdist
+"""
+
+
+from setuptools import setup
+
+
+setup(
+    name         = 'aptask',
+    version      = '0.1.0',
+    description  = 'Asynchronous Parallel Task Queue',
+    url          = 'https://github.com/zhester/aptask',
+    author       = 'Zac Hester',
+    author_email = 'zac.hester@gmail.com',
+    license      = 'BSD',
+    packages     = [ 'aptask' ],
+    scripts      = [ 'scripts/aptaskd.py', 'scripts/aptask.py' ],
+    data_files   = [ ( 'config', 'conf/aptaskd.json' ) ],
+    zip_safe     = False
+)
+
